78c0683db2b4ae81545b19ba4ebb561150d56fd7
oculi <anclarke04@icloud.com>
2026-04-29 13:52:51 -0700

    This code has an error in it

--- writerdeckProgram.py
+++ writerdeckProgram.py
@@ -1,3 +1,5 @@
 print("Hello, Welcome to the Writer Deck Program")
 print()
 name = input('Who is input a text entry: ')
+
+entry_prompt = input("Hello", name, ", are you going to input text, 'Y' or 'N': ")
