90bb26ac22c765c3c5ebd51ee8b82e84ea3d2af5
oculi <anclarke04@icloud.com>
2026-04-30 13:22:06 -0700

    Main structure revised and updated

--- writerdeckProgram.py
+++ writerdeckProgram.py
@@ -1,5 +1,25 @@
-print("Hello, Welcome to the Writer Deck Program")
-print()
-name = input('Who is input a text entry: ')
+def greeting():
+    print("Hello!, Welcome to the WriterDeck Program by ocul1")
+    print()
+greeting()
 
-entry_prompt = input("Hello", name, ", are you going to input text, 'Y' or 'N': ")
+def name_gathering():
+    name = input("Name of person submitting entry: ")
+    print()
+name_gathering()
+
+def writer_deck_main():
+    
+
+
+
+def run_again():
+    answer = input("Would you like to run this program again, 'Y' or 'N': ").lower()    
+    while answer not in ("y", "n"):
+        answer = input("Input Error: Please enter y or n: ").lower()
+    if answer == 'y':
+        return True
+    else:
+        return False 
+
+
